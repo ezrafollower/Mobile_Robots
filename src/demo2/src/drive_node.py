51204553c495d65719aca365ee45953a459a23cc
kiat <DiaboloKiat@gmail.com>
2019-11-01 11:44:04 +0800

    finish demo2

--- src/demo2/src/drive_node.py
+++ src/demo2/src/drive_node.py
@@ -7,21 +7,49 @@ def main():
 
     pub_r = rospy.Publisher("right_pwm", Int16, queue_size = 10)
     pub_l = rospy.Publisher("left_pwm",  Int16, queue_size = 10)
+    trim = -11
 
     while not rospy.is_shutdown():
         try:
             pwm_r = int(raw_input("user's right:"))
         except ValueError:
-            rospy.logerr("Invalid input")
+            rospy.logerr("Invalid input, set default value 0")
             pwm_r = 0
         try:
             pwm_l = int(raw_input("user's left:"))
         except ValueError:
-            rospy.logerr("Invalid input")
+            rospy.logerr("Invalid input, set default value 0")
             pwm_l = 0
+        
+        # Go forward input_r=input_l=111
+        if (pwm_r == pwm_l):
+            pwml_raw = pwm_l
+            pwmr_raw = pwm_r
+            if(pwm_r != 0 ):
+                pwm_r = pwmr_raw + trim
+            if(pwm_l != 0 ):
+                pwm_l = pwml_raw - trim
+            print pwm_l, pwm_r
+            pub_r.publish(Int16(pwm_r))
+            pub_l.publish(Int16(pwm_l))
+            rospy.sleep(6.5)
+	
+        # Other motion
+	else:
+            pwml_raw = pwm_l
+            pwmr_raw = pwm_r
+            if(pwm_r != 0 ):
+                pwm_r = pwmr_raw + trim
+            if(pwm_l != 0 ):
+                pwm_l = pwml_raw - trim
+            print pwm_l, pwm_r
+            pub_r.publish(Int16(pwm_r))
+            pub_l.publish(Int16(pwm_l))
+            rospy.sleep(4.0)
 
-        pub_r.publish(Int16(pwm_r))
-        pub_l.publish(Int16(pwm_l))
+        pub_r.publish(Int16(0))
+        pub_l.publish(Int16(0))
+
 
 if __name__ == "__main__":
     main()
