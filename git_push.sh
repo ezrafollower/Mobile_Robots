c4592588b5981381a52a97c6c6a90b2b2e08791f
ezrafollower <ezrafollower@gmail.com>
2021-12-22 15:57:13 +0800

    update

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,5 @@
 git add .
 git commit -m "update"
-git config --global user.email "ezrafollower@gmail.com"
-git config --global user.name "ezrafollower"
+#git config --global user.email "ezrafollower@gmail.com"
+#git config --global user.name "ezrafollower"
 git push origin master
